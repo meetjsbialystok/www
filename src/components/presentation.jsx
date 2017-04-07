a20c6c83ee9846642f730c524872eaba6a329b6e
Hubert Zub <hubert.zub@softwarehut.pl>
2017-04-07 12:28:58 +0200

    Items update

--- src/components/presentation.jsx
+++ src/components/presentation.jsx
@@ -3,6 +3,25 @@ import React from 'react';
 export default class Presentation extends React.Component {
   render() {
     const { presentation } = this.props;
+
+    let presentationLink = null;
+    if (presentation.link_pres) {
+      presentationLink = (
+        <a href={presentation.link_pres} target="_blank" className="c-presentation--link">
+          <i className="icon icon-dropbox"></i> Prezentacja
+        </a>
+      );
+    }
+
+    let repoLink = null;
+    if (presentation.link_repo) {
+      repoLink = (
+        <a href={presentation.link_repo} target="_blank" className="c-presentation--link">
+          <i className="icon icon-github"></i> Repozytorium
+        </a>
+      );
+    }
+
     return (
       <article className="c-presentation">
         <div className="c-presentation--title">
@@ -10,12 +29,7 @@ export default class Presentation extends React.Component {
         </div>
         <div className="c-presentation--author">{presentation.author}, data: {presentation.date}</div>
         <div className="c-presentation--links">
-          <a href={presentation.link_pres} target="_blank" className="c-presentation--link">
-            <i className="icon icon-dropbox"></i> Prezentacja
-          </a>
-          <a href={presentation.link_repo} target="_blank" className="c-presentation--link">
-            <i className="icon icon-github"></i> Repozytorium
-          </a>
+          <span className="c-presentation--links-label">Linki:</span> {presentationLink} {repoLink}
         </div>
       </article>
     );
