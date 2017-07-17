0302801b45541ffc68c4585c8b52fcb749bc48d3
Hubert Zub <hubert.zub@softwarehut.pl>
2017-07-17 11:04:13 +0200

    Conference site updates

--- src/summer-edition/components/agenda/pageAgenda.jsx
+++ src/summer-edition/components/agenda/pageAgenda.jsx
@@ -11,15 +11,15 @@ export default class PageAgenda extends React.Component {
 
                     <div className="agenda__container">
                         <div className="agenda__box-wrapper">
-                            <div className="agenda__category-name color--primary">TBA</div>
+                            <div className="agenda__category-name color--primary">Strefa egzotyki</div>
                             <div className="agenda__box">
                                 <div className="agenda__box__photo">
-                                    <img src={require('../../assets/p_tba.png')} />
+                                    <img src={require('../../assets/p_kuba.png')} />
                                 </div>
                                 <div className="agenda__box__content bgcolor--tertiary">
-                                    <div className="agenda__box__name color--primary">TBA</div>
-                                    <div className="agenda__box__company color--white">TBA</div>
-                                    <div className="agenda__box__title color--white">TBA!</div>
+                                    <div className="agenda__box__name color--primary">Jakub Gutkowski</div>
+                                    <div className="agenda__box__company color--white">Warszawa // blog.gutek.pl</div>
+                                    <div className="agenda__box__title color--white">Elixir miłości</div>
                                 </div>
                             </div>
                         </div>
@@ -32,20 +32,20 @@ export default class PageAgenda extends React.Component {
                                 <div className="agenda__box__content bgcolor--tertiary">
                                     <div className="agenda__box__name color--primary">Marcin Grzywaczewski</div>
                                     <div className="agenda__box__company color--white">Wrocław</div>
-                                    <div className="agenda__box__title color--white">TBA</div>
+                                    <div className="agenda__box__title color--primary">Nazwa prezentacji już wkrótce</div>
                                 </div>
                             </div>
                         </div>
                         <div className="agenda__box-wrapper">
-                            <div className="agenda__category-name color--primary">Strefa egzotyki</div>
+                            <div className="agenda__category-name color--primary">&nbsp;</div>
                             <div className="agenda__box">
                                 <div className="agenda__box__photo">
-                                    <img src={require('../../assets/p_kuba.png')} />
+                                    <img src={require('../../assets/p_tba.png')} />
                                 </div>
                                 <div className="agenda__box__content bgcolor--tertiary">
-                                    <div className="agenda__box__name color--primary">Jakub Gutkowski</div>
-                                    <div className="agenda__box__company color--white">blog.gutek.pl</div>
-                                    <div className="agenda__box__title color--white">Elixir miłości</div>
+                                    <div className="agenda__box__name color--primary"></div>
+                                    <div className="agenda__box__company color--white"></div>
+                                    <div className="agenda__box__title color--primary">Informacje o prelegencie już wkrótce</div>
                                 </div>
                             </div>
                         </div>
--- src/summer-edition/components/register/pageRegister.jsx
+++ src/summer-edition/components/register/pageRegister.jsx
@@ -9,7 +9,9 @@ export default class PageRegister extends React.Component {
                 <div className="section">
                     <div className="columns">
                         <div className="column">
-                            <a className="register--button" href="https://meetjsbialystok.evenea.pl/" target="_blank">Zarejestruj się</a>
+                            <div className="register__date color--white">26 sierpnia 2017, 12:00</div>
+                            <div className="register__place color--white">Aula Duża Wyższej Szkoły Finansów i Zarządzania</div>
+                            <a className="register__button" href="https://meetjsbialystok.evenea.pl/" target="_blank">Zarejestruj się</a>
                         </div>
                     </div>
                 </div>
